b9ea59c967374428e1a70c614482a3be4ce1ad11
neol-d2022 <neol_d2022@outlook.com>
2016-11-24 16:55:23 +0800

    Initial commit

---
+++ LCD_Controller/sketch_nov24c.ino.ino
@@ -0,0 +1,116 @@
+const int Hpins[] = { 2,  3,  4,  5,  6,  7,  8,  9}; //-
+const int Vpins[] = {10, 11, 12, 13, A0, A1, A2, A3}; //+
+
+//10 11 12 13  6  7  8  9
+// 2  3  4  5 A0 A1 A2 A3
+
+void clearLED() {
+  int i;
+  for(i = 0; i < 8; i++) {
+    digitalWrite(Vpins[i], LOW);
+    digitalWrite(Hpins[i], LOW);
+  }
+}
+
+void printLED(unsigned char *arr, int t) { // t = 1 for 8ms
+  int i, j, k;
+  unsigned char v, l;
+  for(k = 0; k < t; k++) {
+    for(i = 0; i < 8; i++) {    //Vertical
+      digitalWrite(Vpins[(i + 7) % 8], LOW);
+      digitalWrite(Vpins[i], HIGH);
+      v = arr[i];
+      
+      for(j = 0; j < 8; j++) {  //Horizental
+        l = v & (1 << (7 - j));
+        l = (l > 0) ? (LOW) : (HIGH);
+        digitalWrite(Hpins[j], l);
+      }
+      delay(1);
+    }
+  }
+  clearLED();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i = 0; i < 8; i++) {
+    pinMode(Hpins[i], OUTPUT);
+    pinMode(Vpins[i], OUTPUT);
+  }
+  
+}
+
+unsigned char test1[] = {
+  0xC3,
+  0xC3,
+  0xC3,
+  0xFF,
+  0xFF,
+  0xC3,
+  0xC3,
+  0xC3
+};
+
+unsigned char test2[] = {
+  0x7E,
+  0x7E,
+  0x40,
+  0x7E,
+  0x7E,
+  0x40,
+  0x7E,
+  0x7E
+};
+
+unsigned char test3[] = {
+  0x60,
+  0x60,
+  0x60,
+  0x60,
+  0x60,
+  0x60,
+  0x7E,
+  0x7E
+};
+
+unsigned char test4[] = {
+  0xFF,
+  0xFF,
+  0xC3,
+  0xC3,
+  0xC3,
+  0xC3,
+  0xFF,
+  0xFF
+};
+
+unsigned char zero[] = {
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00
+};
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printLED(test1, 120);
+  printLED(zero, 5);
+  printLED(test2, 120);
+  printLED(zero, 5);
+  printLED(test3, 120);
+  printLED(zero, 5);
+  printLED(test3, 120);
+  printLED(zero, 5);
+  printLED(test4, 120);
+  printLED(zero, 5);
+
+  /*for(int i = 0; i < 8; i++) {
+    digitalWrite(Vpins[i], HIGH);
+    digitalWrite(Hpins[i], LOW);
+  }*/
+}
---
+++ SecondaryBoard/sketch_nov24d.ino.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11,  5,  4,  3,  2);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.print("Hello world!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(0, 1);
+  lcd.print(millis() / 1000);
+
+  if(analogRead(A4) > 512) {
+    tone(8, 262);
+  }
+  else {
+    noTone(8);
+  }
+  delay(100);
+}
